f73006c88b10a4138a1c6a45262b9651082ed994
Kevin Michael Schott <k.schott95@gmail.com>
2022-06-19 21:20:19 +0200

    Better SEO structure; margin on image was wrong

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,37 +1,36 @@
-<header role="heading" aria-label="profile" class="flex flex-col w-full h-full <md:text-justify text-left">
-  <section class="flex md:flex-row flex-col flex-nowrap items-center">
-    <div class="relative mr-md">
-      <img
-        src="favicon.jpg"
-        alt="Kevin Michael Schott"
-        id="profile-pic"
-        width="100%"
-        height="100%"
-        class="rounded-1/2 w-6rem pressable std-hover--glow std-hover--scale cursor-pointer"
-        tabindex="0"
-      />
-    </div>
-    <h1 tabindex="0" class="<md:text-center">
-      Hey there, I am <span class="text-secondary">Kevin</span>&nbsp;<span class="wave">👋</span>
-    </h1>
-  </section>
-  <section>
-    <p>
-      As an independent software developer & IT consultant from Germany, I publish a lot of my work on <a
-        target="_blank"
-        href="https://github.com/kevinXmichael">GitHub</a
-      >.
-    </p>
-    <p>
-      I love to work with modern web technologies and strongly believe that Web 3.0 is the future. You can find more
-      information about me on
-      <a target="_blank" href="https://www.linkedin.com/in/kmschott/">LinkedIn</a> and in my
-      <a target="_blank" href="/CV.pdf">CV</a>. If you think we can work together, feel free to send me an
-      <a href="mailto:hello@kms695.de">Email</a>.
-    </p>
-    <p>In my freetime I enjoy walking my dog, sports, movies, reading mangas and play video games.</p>
-  </section>
+<header class="flex md:flex-row flex-col flex-nowrap items-center" role="heading" aria-label="profile">
+  <div class="relative md:mr-md">
+    <img
+      src="favicon.jpg"
+      alt="Kevin Michael Schott"
+      id="profile-pic"
+      width="100%"
+      height="100%"
+      class="rounded-1/2 w-6rem pressable std-hover--glow std-hover--scale cursor-pointer"
+      tabindex="0"
+    />
+  </div>
+  <h1 tabindex="0" class="<md:text-center">
+    Hey there, I am <span class="text-secondary">Kevin</span>&nbsp;<span class="wave">👋</span>
+  </h1>
 </header>
+<section>
+  <p>
+    As an independent software developer & IT consultant from Germany, I publish a lot of my work on <a
+      target="_blank"
+      href="https://github.com/kevinXmichael">GitHub</a
+    >.
+  </p>
+  <p>
+    I love to work with modern web technologies and strongly believe that Web 3.0 is the future. You can find more
+    information about me on
+    <a target="_blank" href="https://www.linkedin.com/in/kmschott/">LinkedIn</a> and in my
+    <a target="_blank" href="/CV.pdf">CV</a>. If you think we can work together, feel free to send me an
+
+    <a href="mailto:hello@kms695.de">Email</a>.
+  </p>
+  <p>In my freetime I enjoy walking my dog, sports, movies, reading mangas and play video games.</p>
+</section>
 
 <style>
   p {
