f858ad4173a11f43a1f924152d08dc28b24f55d5
Kevin Michael Schott <k.schott95@gmail.com>
2022-06-20 10:25:33 +0200

    Small text cleanup

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,29 +11,32 @@
     />
   </div>
   <h1 tabindex="0" class="<md:text-center">
-    Hey there, I am <span class="text-secondary">Kevin</span>&nbsp;<span class="wave">👋</span>
+    Hey there, it's <span class="text-secondary">Kevin</span>&nbsp;<span class="wave">👋</span>
   </h1>
 </header>
 <section>
   <p>
-    As an independent software developer & IT consultant from Germany, I publish a lot of my work on <a
+    I am an independent software developer & IT consultant from Germany and publish a lot of stuff on <a
       target="_blank"
       href="https://github.com/kevinXmichael">GitHub</a
     >.
   </p>
   <p>
-    I love to work with modern web technologies and strongly believe that Web 3.0 is the future. You can find more
-    information about me on
+    As a tech enthusiast I love to work with modern web technologies and strongly believe that web 3.0 is the future.
+  </p>
+  <p>
+    You can find more information about me on
     <a target="_blank" href="https://www.linkedin.com/in/kmschott/">LinkedIn</a> and in my
     <a target="_blank" href="/CV.pdf">CV</a>. If you think we can work together, feel free to send me an
-
     <a href="mailto:hello@kms695.de">Email</a>.
   </p>
-  <p>In my freetime I enjoy walking my dog, sports, movies, reading mangas and play video games.</p>
 </section>
 
 <style>
-  p {
+  p,
+  h1,
+  h2,
+  h3 {
     @apply mt-md;
   }
 </style>
